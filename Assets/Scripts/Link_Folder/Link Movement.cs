5aa87fab35725e2ea3f633cf2e8a03b516609350
davidfb1305 <149385933+davidfb1305@users.noreply.github.com>
2024-01-23 19:21:42 +0100

    movimiento

--- Assets/Scripts/Link_Folder/Link Movement.cs
+++ Assets/Scripts/Link_Folder/Link Movement.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class LinkMovement : MonoBehaviour
 {
+    [SerializeField]
+    private Transform _transform;
+    [SerializeField]
+    private Rigidbody2D rb;
+    [SerializeField]
+    private float _speed = 1.0f;
+    [SerializeField]
+    private InputActionReference _Input;
+    Vector2 actualspeed = Vector2.zero;
+    Vector2 targetspeed = Vector2.zero;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        Vector2 _vectormovimiento = GetInput();
+
+        if (_vectormovimiento != Vector2.zero)
+        {
+            actualspeed = rb.velocity;
+            targetspeed = _speed * _vectormovimiento;
+        }
+        else
+        {
+            actualspeed = Vector2.zero;
+            targetspeed = Vector2.zero;
+        }
+
+
+        Vector3 finalspeed = (targetspeed - actualspeed);
+        finalspeed.z = 0;
+        Debug.Log(finalspeed);
+        //   rb.AddForce(finalspeed * rb.mass / Time.fixedDeltaTime);
+        transform.position += finalspeed * Time.deltaTime;
+    }
+    private void OnEnable()
+    {
+        _Input.action.Enable();
+    }
+    private void OnDisable()
+    {
+        _Input.action.Disable();
+
+    }
+    public Vector2 GetInput()
+    {
+        return _Input.action.ReadValue<Vector2>();
     }
 }
