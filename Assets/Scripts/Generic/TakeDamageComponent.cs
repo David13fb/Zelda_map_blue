a046596e9358650b06ba7051dab7ae2b09cd6be9
Nerea E <nereaesp@ucm.es>
2024-01-28 17:52:57 +0100

    color loop 1

--- Assets/Scripts/Generic/TakeDamageComponent.cs
+++ Assets/Scripts/Generic/TakeDamageComponent.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
 using UnityEngine;
+
+
 
 public class TakeDamageComponent : MonoBehaviour
 {
     //references
     HP_manager _hpManager;
-    Rigidbody2D _rb;
+    Rigidbody2D _rb;            
     SpriteRenderer _spriteRenderer;
     [SerializeField]Transform _linkTransform; //taken to push everything in links looking direction
-
+   
     //parameters
     private Vector3 _pushDirection;
+    [SerializeField]
+    private int _colorQuantity = 0;
+    private int _loopIterations = 0;
 
     // Comented code is a invul system
 
@@ -26,6 +32,7 @@ public class TakeDamageComponent : MonoBehaviour
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _hpManager = GetComponent<HP_manager>();
         //_stopwatch.Start();
+    
     }
     public void TakeDamage(int damage) 
     { 
@@ -34,6 +41,7 @@ public class TakeDamageComponent : MonoBehaviour
             if (_hpManager != null && _spriteRenderer != null && _rb != null)
             {
                 _hpManager.changeCurrentHealth(-damage); //damage taken is a negative, damage is a positive value
+                _loopIterations = _colorQuantity;
                 ColorLoop();
                 _pushDirection = -_linkTransform.up;
                 _rb.AddForce(_pushDirection);
@@ -42,10 +50,16 @@ public class TakeDamageComponent : MonoBehaviour
         //}
     }
 
-    private void ColorLoop()
+    void ColorLoop()
     {
-
+        
+     
+        _spriteRenderer.color = Random.ColorHSV();
+           
+        _spriteRenderer.color = UnityEngine.Color.white;
     }
 
+   
+    
 
 }
