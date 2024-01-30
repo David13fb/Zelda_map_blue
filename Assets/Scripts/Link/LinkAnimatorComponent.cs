9e3d69836a6cc46048d208d40d0ff1b81304aafa
Eric11196 <izandeve@ucm.es>
2024-01-30 17:16:22 +0100

    arreglada la camara en la tienda

--- Assets/Scripts/Link/LinkAnimatorComponent.cs
+++ Assets/Scripts/Link/LinkAnimatorComponent.cs
@@ -7,23 +7,12 @@ public class LinkAnimatorComponent : MonoBehaviour
     [SerializeField]
     private Animator _animator;
 
-    //private Rigidbody2D _rb;
-
     // Start is called before the first frame update
     void Start()
     {
-        //_rb = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        /*
-        _animator.SetInteger("xMove", (int)Mathf.Round(_rb.velocity.x));
-        _animator.SetInteger("yMove", (int)Mathf.Round(_rb.velocity.y));
-        */
-    }
 
     public void UpdateMoveAnimation(Vector2 input)
     {
