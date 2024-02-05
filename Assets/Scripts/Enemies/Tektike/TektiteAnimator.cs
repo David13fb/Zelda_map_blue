730f5800870da115f67037294150cf2d921c3813
Nerea E <nereaesp@ucm.es>
2024-02-05 20:44:36 +0100

    animation+animation machines+ parto of tektike animation script commented

---
+++ Assets/Scripts/Enemies/Tektike/TektiteAnimator.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class TektiteAnimator : MonoBehaviour
+{/*
+    [SerializeField]
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    static void Cloud1()
+    {
+
+        _animator.SetBool("Cloud1", true);
+        _animator.SetBool("Cloud2", false);
+
+
+
+    }
+    static void Cloud2()
+    {
+
+        _animator.SetBool("Cloud1", false);
+        _animator.SetBool("Cloud2", true);
+
+
+
+    }
+
+    static void Cloud3() 
+    {
+
+
+        _animator.SetBool("Cloud1", false);
+        _animator.SetBool("Cloud2", false);
+
+
+
+    }
+
+    //vector that the IA will pass, its the position
+    static void Tektike(Vector3 movement)
+    {
+        //if its on the groud
+        if ()
+        {
+
+           
+        }
+        //if its on the sky
+        else
+        { 
+        
+        
+        
+        }
+    
+    
+    
+    }
+
+    */
+}
